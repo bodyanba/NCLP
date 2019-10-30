67ad332061b09b275d11b43f2260549b6aea8a11
bodyanba <galkinbogdan@gmail.com>
2019-10-30 15:04:43 +0300

    news block

--- js/main.js
+++ js/main.js
@@ -11,64 +11,18 @@ $(document).ready(function() {
             $('.fullscreen.section-home .container').css('margin-top', containerPos);
 		});
 	};
-
-    //---------- Sticky Header JS
-    function sticky_header(){
-        $(document).ready(function(){
-            $('#header').each(function() {
-                var homeHeight = $('#home').height() -1,
-                    pageHeaderHeight = $('#page-header').height() -1;
-                var headerHeight = $('#header').height();
-                $(".header-2").each(function(){
-                    $(this).after("<div id='hidden-header'></div>");
-                });
-                $(window).scroll(function(){
-                    if ($('#home').length == 1 ){
-                        if ($(window).scrollTop() > homeHeight) {
-                            $(".header").addClass("header-bg");
-                            $(".header-2").addClass("sticky");
-                            $(".header-2").each(function(){
-                                $("#hidden-header").css('height', headerHeight);
-                            });
-                        } else {
-                            $(".header").removeClass("header-bg");
-                            $(".header-2").removeClass("sticky");
-                            $(".header-2").each(function(){
-                                $("#hidden-header").css('height', 0);
-                            });
-                        }
-                    } else {
-                        if ($(window).scrollTop() > pageHeaderHeight) {
-                            $(".header").addClass("header-bg");
-                            $(".header-2").addClass("sticky");
-                            $(".header-2").each(function(){
-                                $("#hidden-header").css('height', headerHeight);
-                            });
-                        } else {
-                            $(".header").removeClass("header-bg");
-                            $(".header-2").removeClass("sticky");
-                            $(".header-2").each(function(){
-                                $("#hidden-header").css('height', 0);
-                            });
-                        }
-                    }
-                });
-            });
-        });
-    };
-     
-    $(document).ready(function(){
+    full_screen_section();
+    $(window).resize(function(){
         full_screen_section();
-        sticky_header();
     });
 
-    $(window).resize(function(){
-		full_screen_section();
-	});
-
-    $(window).load(function (){
-        full_screen_section();
-        sticky_header();
+    //---------- Sticky Header JS
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() >= 1) {
+            $('#header').addClass("header-bg");
+        } else {
+            $('#header').removeClass("header-bg");
+        };
     });
     
 	//---- tooltip
@@ -106,6 +60,18 @@ $(document).ready(function() {
 		itemsTablet: [600, 1],
 		autoHeight: true,
 	});
+    $('.custom-carousel-2').owlCarousel({
+        stopOnHover: true,
+        autoPlay: 6000,
+        navigation: false,
+        pagination: true,
+        lazyLoad: true,
+        items: 3,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [1000, 2],
+        itemsTablet: [600, 1],
+        autoHeight: true,
+    });
 
     $('.clock-countdown').downCount({
         date: $('.site-config').attr('data-date'),
