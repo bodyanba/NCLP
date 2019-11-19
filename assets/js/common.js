b0bf3ea4cdf37d8f3462453132dca7deced8f88e
bodyanba <galkinbogdan@gmail.com>
2019-11-19 17:44:11 +0300

    8

--- assets/js/common.js
+++ assets/js/common.js
@@ -36,18 +36,4 @@ $(document).ready(function () {
     footer();
   });
 
-  var anim;
-  var animData = {
-      container: document.getElementById('preloader'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      rendererSettings: {
-          progressiveLoad: false
-      },
-      path: '../json/nclp_logo.json'
-  };
-  anim = bodymovin.loadAnimation(animData);
-  anim.setSpeed(1);
-
 });
---
+++ assets/js/preloader.js
@@ -0,0 +1,13 @@
+var anim;
+var animData = {
+  container: document.getElementById('preloader'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  rendererSettings: {
+      progressiveLoad: false
+  },
+  path: '../json/nclp_logo.json'
+};
+anim = bodymovin.loadAnimation(animData);
+anim.setSpeed(1);
