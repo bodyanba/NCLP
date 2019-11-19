9c55615c2d4bc2040800c60d3fe8d77c68a6c919
bodyanba <galkinbogdan@gmail.com>
2019-11-19 17:37:19 +0300

    8

--- assets/js/common.js
+++ assets/js/common.js
@@ -21,11 +21,16 @@ $(document).ready(function () {
     }
   });
 
+  $('.scroll-to').on('click', function(e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({ scrollTop: $(target).offset().top }, 1000, "easeInOutExpo");
+  });
+
   function footer() {
     var height = $(".page-footer").outerHeight();
     $(".footer-wrapper").outerHeight(height);
   };
-
   footer();
   $(window).on("resize", function() {
     footer();
