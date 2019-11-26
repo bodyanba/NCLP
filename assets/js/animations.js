d003ae225b24e2037f3b0281db22153a7b82f07e
bodyanba <galkinbogdan@gmail.com>
2019-11-26 17:05:07 +0300

    animations 4

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,47 +1,63 @@
-if ( document.getElementById('patternLines')) {
+if ( document.getElementById('anim-lines')) {
 	var anim;
 	var animData = {
-			container: document.getElementById('patternLines'),
+			container: document.getElementById('anim-lines'),
 			renderer: 'svg',
 			loop: true,
 			autoplay: true,
 			rendererSettings: {
 				progressiveLoad: false
 			},
-			path:'../json/nclp_lines.json'
+			path:'../json/anim-lines.json'
 	};
 	anim = bodymovin.loadAnimation(animData);
 	anim.setSpeed(1);
 };
 
-if (document.getElementById('patternWhite')) {
+if (document.getElementById('anim-white')) {
 	var anim;
 	var animData = {
-			container: document.getElementById('patternWhite'),
+			container: document.getElementById('anim-white'),
 			renderer: 'svg',
 			loop: true,
 			autoplay: true,
 			rendererSettings: {
 				progressiveLoad: false
 			},
-			path:'../json/nclp_pattern_white.json'
+			path:'../json/anim-white.json'
 	};
 	anim = bodymovin.loadAnimation(animData);
 	anim.setSpeed(0.5);
 };
 
-if(document.getElementById('sphereWhite')) {
+if(document.getElementById('anim-sphere')) {
 	var anim;
 	var animData = {
-			container: document.getElementById('sphereWhite'),
+			container: document.getElementById('anim-sphere'),
 			renderer: 'svg',
 			loop: true,
 			autoplay: true,
 			rendererSettings: {
 				progressiveLoad: false
 			},
-			path:'../json/nclp_sphere_white.json'
+			path:'../json/anim-sphere.json'
 	};
 	anim = bodymovin.loadAnimation(animData);
 	anim.setSpeed(0.5);
+};
+
+if(document.getElementById('anim-header')) {
+	var anim;
+	var animData = {
+			container: document.getElementById('anim-header'),
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			rendererSettings: {
+				progressiveLoad: false
+			},
+			path:'../json/anim-header.json'
+	};
+	anim = bodymovin.loadAnimation(animData);
+	anim.setSpeed(1);
 };
